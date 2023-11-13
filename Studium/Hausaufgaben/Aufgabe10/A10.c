f0ad7481b23f731781b97d09812bef5932678fe6
Josh <jnievelstein@yahoo.com>
2023-11-13 21:48:00 +0100

    Beginning
    Aufgabe 10

--- Studium/Hausaufgaben/Aufgabe10/A10.c
+++ Studium/Hausaufgaben/Aufgabe10/A10.c
@@ -2,10 +2,23 @@
 
 int main() {
     int var;
+    char wort;
+    int counter =0;
     int varCounter = 0;
     
     do
-    {
+    {   
+        // w”rter Z„hlen
+        // wenn ber 50 zeichen -> repeat
+        // wenn weniger -> eingabe ausgeben
+
+        // eingabe umgekehrt ausgeben
+        // Groábuchstaben z„hlen
+        // vokale z„hlen
+        // nur die konsonanten ausgeben
+        // kompressionsrate berechnen und ausgeben
+
+        
         printf("Zahl eingeben: ");
         scanf("%d", &var);
         
---
+++ Studium/Hausaufgaben/Aufgabe11/A11.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main () {
+    int c1 = 12;
+    float fzahl = 56.2872;
+    printf("%.2f\n", fzahl);
+
+    printf("%c\n", (c1*6));
+}
